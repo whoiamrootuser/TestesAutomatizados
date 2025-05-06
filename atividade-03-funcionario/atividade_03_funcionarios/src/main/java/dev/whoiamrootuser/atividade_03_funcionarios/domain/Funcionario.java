8c74eafc8c08ffc2af52cd0cb020b530d6ba3168
Joao Henrique <whoiamrootuser@proton.me>
2025-05-05 21:07:07 -0300

    feat: add Funcionario class with payment calculation and validation rules

---
+++ atividade-03-funcionario/atividade_03_funcionarios/src/main/java/dev/whoiamrootuser/atividade_03_funcionarios/domain/Funcionario.java
@@ -0,0 +1,74 @@
+package dev.whoiamrootuser.atividade_03_funcionarios.domain;
+
+public class Funcionario {
+    private static final double SALARIO_MINIMO = 1518.00;
+    private static final double SALARIO_MAXIMO = 100000.00;
+   
+    private String nome;
+    private int horasTrabalhadas;
+    private double valorHora;
+
+    public Funcionario(String nome, int horasTrabalhadas, double valorHora) {
+        this.nome = nome;
+        this.horasTrabalhadas = validaHorasTrabalhadas(horasTrabalhadas);
+        this.valorHora = validaValorHora(valorHora);
+    }
+
+    public double calcularPagamento() {
+        double pagamento = horasTrabalhadas * valorHora;
+        if (pagamento < SALARIO_MINIMO || pagamento > SALARIO_MAXIMO) {
+            throw new IllegalArgumentException("O pagamento deve ser maior ou igual ao salário mínimo de R$ 1518.00.");
+        }
+        return pagamento;
+    }
+
+    private int validaHorasTrabalhadas(int horasTrabalhadas) {
+        if (horasTrabalhadas < 20 || horasTrabalhadas > 40) {
+            throw new IllegalArgumentException("O número de horas trabalhadas por funcionários próprios deve ser entre 20 e 40.");
+        }
+        return horasTrabalhadas;
+    }
+
+    private double validaValorHora(double valorHora) {
+        double valorMinimo = SALARIO_MINIMO * 0.04;
+        double valorMaximo = SALARIO_MINIMO * 0.10;
+        if (valorHora < valorMinimo || valorHora > valorMaximo) {
+            throw new IllegalArgumentException("O valor da hora deve ser entre 4% e 10% do salário mínimo.");
+        }
+        return valorHora;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getHorasTrabalhadas() {
+        return horasTrabalhadas;
+    }
+
+    public void setHorasTrabalhadas(int horasTrabalhadas) {
+        this.horasTrabalhadas = validaHorasTrabalhadas(horasTrabalhadas);
+    }
+
+    public double getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(double valorHora) {
+        this.valorHora = validaValorHora(valorHora);
+    }
+
+
+
+//     As regras para o cálculo do pagamento são as seguintes:
+// ● O valor do pagamento dos funcionários deve ser maior ou igual ao valor atual do salário mínimo,
+// que corresponde a R$ 1518.00. O salário não pode ultrapassar o teto de 100000.00
+// ● Os funcionários podem trabalhar por no máximo 40 horas. A carga horária mínima é de 20 horas.
+// ● O valor por hora precisa ser entre 4% e 10% do salário mínimo.
+// ● O valor das despesas adicionais não pode ultrapassar R$ 1000.00.
+
+}
